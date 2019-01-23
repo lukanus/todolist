83ea25ec4fe6f5caa83f3de59d5db920cf3237fa
Udalat <46577482+Udalat@users.noreply.github.com>
2019-01-23 19:27:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload= function()
+{
+var a= document.getElementById("radd").addEventListener("click", myFunction);
+
+function myFunction()
+{
+    var li = document.createElement("li");
+    var txt = document.getElementById("txt").value;
+    var node = document.createTextNode(txt);
+    li.appendChild(node);
+    var ul = document.getElementById("todo");
+    ul.appendChild(li);
+}
+    };
