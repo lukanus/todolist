19e9271569bfedf1d2431c19a1f281394563fb3c
Łukasz Miłkowski <lukasz@platoteam.com>
2019-01-24 07:17:33 +0100

    Changeset 1

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-window.onload= function()
-{
-var a= document.getElementById("radd").addEventListener("click", myFunction);
+window.onload = function () {
+    var a = document.getElementById("radd").addEventListener("click", myFunction);
+};
 
-function myFunction()
-{
+function myFunction() {
     var li = document.createElement("li");
     var txt = document.getElementById("txt").value;
     var node = document.createTextNode(txt);
     li.appendChild(node);
     var ul = document.getElementById("todo");
     ul.appendChild(li);
-   li.onclick = myDelete;
-   function myDelete()
-   {
-       li.remove();
-   }
-    
+    li.onclick = myDelete;
 }
-};
 
+
+function myDelete(ev) {
+    console.log(ev); // coś z tym trzeba będzie zrobić
+    li.remove();
+}
